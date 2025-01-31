8ff0a77e0dbbd07d0a190611d3dea689ef3d464c
jojobuzz <baazizyoucef242431578002@gmail.com>
2025-01-31 23:33:04 +0100

    Update calendar.ts
    so that my name appears

--- src/commands/calendar.ts
+++ src/commands/calendar.ts
@@ -1,20 +1,27 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import Reminder from "../schemas/reminder";
+import { ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import Reminder, { IReminder, ReminderType } from "../schemas/reminder";
+
+
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName("calendar")
-        .setDescription("View all upcoming events"),
-    async execute(interaction: ChatInputCommandInteraction) {
-        const events = await Reminder.find().sort({ date: 1 });
+  data: new SlashCommandBuilder()
+  .setName("calendar")
+  .setDescription("View all upcoming events"),
 
-        if (!events.length)
-            return await interaction.reply("No upcoming events.");
 
-        const eventList = events.map(event =>
-            `**${event.title}**\n📅 ${event.date ? event.date.toUTCString() : "Unknown Date"}\n🆔 ${event._id}\n${event.description || "No description"}`
-        ).join("\n\n");
+  async  execute(interaction: ChatInputCommandInteraction) {
+    const events = await Reminder.find().sort({ date: 1 });
 
-        await interaction.reply(eventList);
+    if (events.length === 0) {
+        await interaction.reply("No upcoming events.");
+        return;
     }
+
+    const eventList = events.map(event => 
+        `**${event.title}**\n📅 ${event.date ? event.date.toUTCString() : "Unknown Date"}\n🆔 ${event._id}\n${event.description || "No description"}`
+    ).join("\n\n");
+    
+    await interaction.reply(eventList);
+}
+
 };
