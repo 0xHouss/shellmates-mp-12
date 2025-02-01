17a2d9c9084cdba329dd3021714278f0e83710f1
0xHouss <tkthoussam@gmail.com>
2025-02-01 12:29:13 +0100

    switch google calendar from service account to oauth

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,11 +4,18 @@ import { z } from 'zod';
 const configFile = JSON.parse(fs.readFileSync("config.json", 'utf-8'));
 
 const configSchema = z.object({
+    // Bot
     TOKEN: z.string(),
     CLIENT_ID: z.string(),
+
+    // Database
     MONGODB_URI: z.string(),
-    GOOGLE_CALENDAR_ID: z.string(),
     DB_NAME: z.string(), 
+
+    // Google Calendar
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    GOOGLE_CALENDAR_ID: z.string(),
 })
 
 export default configSchema.parse(configFile);
--- src/lib/google-calendar.ts
+++ src/lib/google-calendar.ts
@@ -1,18 +1,40 @@
 import { calendar_v3, google } from "googleapis";
 import config from "./config";
+import fs from "fs";
 
-const calendar = google.calendar('v3');
 const calendarId = config.GOOGLE_CALENDAR_ID;
 
-// Authenticate with the service account
-const auth = new google.auth.GoogleAuth({
-    keyFile: "google-service-account.json",
-    scopes: ['https://www.googleapis.com/auth/calendar'],
-});
+const oauth2Client = new google.auth.OAuth2(
+    config.GOOGLE_CLIENT_ID, 
+    config.GOOGLE_CLIENT_SECRET,
+    'urn:ietf:wg:oauth:2.0:oob'
+);
+
+// Load the token and refresh it if necessary
+function loadToken() {
+    const creds = JSON.parse(fs.readFileSync('google-credentials.json', 'utf8'));
+    oauth2Client.setCredentials(creds);
+
+    // Check if the access token is expired
+    if (creds.expiry_date && creds.expiry_date <= Date.now()) {
+        // Refresh the token
+        oauth2Client.refreshAccessToken((err, creds) => {
+            if (err || !creds)
+                return console.error('Error refreshing access token:', err);
+
+            oauth2Client.setCredentials(creds);
+            // Save the refreshed token
+            fs.writeFileSync('google-credentials.json', JSON.stringify(creds));
+        });
+    }
+}
 
 export async function getEvents(params: Omit<calendar_v3.Params$Resource$Events$List, 'auth' | 'calendarId'>) {
+    loadToken();
+
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
     const response = await calendar.events.list({
-        auth,
         calendarId,
         ...params
     });
@@ -21,8 +43,11 @@ export async function getEvents(params: Omit<calendar_v3.Params$Resource$Events$
 }
 
 export async function createEvent(event: calendar_v3.Schema$Event) {
+    loadToken();
+
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
     const response = await calendar.events.insert({
-        auth,
         calendarId,
         requestBody: event,
     });
@@ -31,8 +56,11 @@ export async function createEvent(event: calendar_v3.Schema$Event) {
 }
 
 export async function removeEvent(eventId: string) {
+    loadToken();
+
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
     await calendar.events.delete({
-        auth,
         calendarId,
         eventId,
     });
